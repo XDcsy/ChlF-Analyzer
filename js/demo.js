490f66a0119f6187ed1dadb00239008ec3e54b6e
neEverett <shengyang.cheng@outlook.com>
2017-10-11 19:50:29 +0800

    改为选择完文件后自动读取文件

--- js/demo.js
+++ js/demo.js
@@ -1,16 +1,15 @@
 
 $(function(){
-	var output = []; //获取文件列表记录
-	var files;
 	var filesNumber = 0;
 	$("#sel").click(function(){
 		$("#files").click();
 	});
 	function handleFileSelect(evt) {
-	    files = evt.target.files; // FileList对象 
+	    var files = evt.target.files; // FileList对象 
 	    for (var i = 0, f; f = files[i]; i++) {
 	        filesNumber++;
   		}
+		processFiles(files);
     }
      document.getElementById('files').addEventListener('change', handleFileSelect, false);
      $("#next").click(function(){
@@ -19,7 +18,6 @@ $(function(){
      	}else{
      		$(".content-edit").hide();
      		$(".cont").addClass("animated slideInRight").show();
-			processFiles(files);
      	}
      });
      $("#next2").click(function(){
