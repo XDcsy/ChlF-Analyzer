a67f94eb6c242f2138da01f593d61f91737c5e3a
neEverett <shengyang.cheng@outlook.com>
2017-10-18 23:00:34 +0800

    优化chart0的性能
    给chart0关闭了动画，关闭了datazoom以提升性能
    减少了网页高度
    去掉了operation area的边框

--- js/chart0.js
+++ js/chart0.js
@@ -1,17 +1,19 @@
 var chart0Options ={
-			dataZoom: [
-            {
-                type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
-                start: 0,      // 左边在 10% 的位置。
-                end: 100         // 右边在 60% 的位置。
-            },
-            {
-                type: 'inside', // 这个 dataZoom 组件是 inside 型 dataZoom 组件
-                start: 0,      // 左边在 10% 的位置。
-                end: 100         // 右边在 60% 的位置。
-            }
-            ],
+//			dataZoom: [
+//            {
+//                type: 'slider', // 这个 dataZoom 组件是 slider 型 dataZoom 组件
+//                start: 0,      // 左边在 10% 的位置。
+//                end: 100         // 右边在 60% 的位置。
+//            },
+//            {
+//                type: 'inside', // 这个 dataZoom 组件是 inside 型 dataZoom 组件
+//                start: 0,      // 左边在 10% 的位置。
+//                end: 100         // 右边在 60% 的位置。
+//            }
+//            ],
 			
+	animation: false,  //关闭动画，提高性能
+	
     toolbox : {
         feature : {
                 brush: {
